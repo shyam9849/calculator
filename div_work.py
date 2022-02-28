d727622fe949efc720f7bc61f2661a92e25e93e7
shyam <shyam.shiwakoti16@gmail.com>
2022-02-27 20:36:18 -0800

    done

--- div_work.py
+++ div_work.py
@@ -1,3 +1,3 @@
 #this is for div
 def div(a,b);
-return a+b
+return a/b
---
+++ maincalc.py
@@ -0,0 +1,15 @@
+#this is by sam
+import add_work
+import sub_work
+import div_work
+import mul_work
+
+a=100
+b=50
+
+total_sum = add_work.add(a,b)
+total_sub = sub_work.sub(a,b)
+total_mul = mul_work.mul(a,b)
+total_div = div_work.div(a,b)
+
+print(f"total sum is {total_sum}. total sub is {total_sub}. total mul is {total_mul}. total div is {total_div}.")
